e3c2e77584aa8f3e54f718fd2e4bf93056c60b90
Joe Thorley <joe@poissonconsulting.ca>
2018-02-04 16:30:14 -0800

    removing dependencies

--- R/namespace.R
+++ R/namespace.R
@@ -1,4 +1,4 @@
-#' @import coda checkr mcmcr
+#' @import checkr mcmcr
 #' @importFrom dplyr anti_join group_by_ filter_ inner_join left_join semi_join slice_ summarise summarise_ ungroup
 #' @importFrom stats coef
 NULL
